98d6546c4531b496c9f766312c3eae578f7b0bac
cnewton <cnewton@tagworldwide.com>
2014-09-24 15:27:55 +0100

    sorted out media queries

--- js/global.js
+++ js/global.js
@@ -18,7 +18,55 @@
 			scroller = new CustomScroll(pageContents, {ease:true, thumbClass:'scrollbar-thumb', trackClass:'scrollbar-track'});
 			scroller.onScroll(onScrolling);
 		}
+		
+		var videos = document.querySelectorAll('.playvideo');
+		for(var i=0; i< videos.length; i++){
+			videos[i].addEventListener('click', onPlayClicked, false);			
+			
+		}
+		
 	};
+	
+	function onPlayClicked(e){
+		var vidBG = document.createElement('div');
+		
+		vidBG.setAttribute('class','videoBg');
+		document.body.appendChild(vidBG);
+		
+		var loader = document.createElement('img');
+		loader.src = 'images/loader.gif';
+		loader.style.position = 'absolute';
+		vidBG.appendChild(loader);
+		loader.onload = function(){
+			loader.style.top = (window.innerHeight*0.5 - loader.offsetHeight*0.5) + "px";
+			loader.style.left = (window.innerWidth*0.5 - loader.offsetWidth*0.5) + "px";
+			vidBG.style.opacity = "1";
+		};
+		
+		var vidIframe = document.createElement('iframe');
+		vidIframe.src = e.target.dataset.url;
+		vidIframe.width = "60%";
+		vidIframe.frameborder = "0";
+		vidIframe.allowFullScreen = "true";
+		//vidIframe.style.visibility = "hidden";
+		vidIframe.setAttribute('class','video');
+		vidBG.appendChild(vidIframe);
+		
+		vidIframe.onload = function(){
+			
+			vidIframe.height = vidIframe.offsetWidth / 1.78;
+			//vidBG.style.visibility = "visible";
+			vidIframe.style.opacity = "1";
+			vidIframe.style.top = (window.innerHeight*0.5 - vidIframe.offsetHeight*0.5) + "px";
+			vidIframe.style.left = (window.innerWidth*0.5 - vidIframe.offsetWidth*0.5) + "px";
+			
+		};
+		
+		vidBG.addEventListener('click',function(){
+			document.body.removeChild(vidBG);
+			vidIframe.src = null;
+		},false);
+	}
 	
 	function onScrolling(scrollup){
 		
